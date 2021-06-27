ec39ac57f4d1e06dbd9a933e8f798c3639f78d3e
htwalid <hadjtaieb.walid@gmail.com>
2021-06-27 23:29:33 +0100

    Update bridge nf trafic

--- vagrant/ubuntu/allow-bridge-nf-traffic.sh
+++ vagrant/ubuntu/allow-bridge-nf-traffic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sysctl net.bridge.bridge-nf-call-iptables=1
+#sysctl net.bridge.bridge-nf-call-iptables=1
+cat >> /etc/sysctl.conf <<EOF
+  net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl -p /etc/sysctl.conf
