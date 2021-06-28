b4227a5337fae9afab53d57a51ffc515700c6b81
htwalid <hadjtaieb.walid@gmail.com>
2021-06-28 17:22:38 +0100

    Update VM resources requirements

--- vagrant/suse/allow-bridge-nf-traffic.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo sysctl net.bridge.bridge-nf-call-iptables=1
--- vagrant/ubuntu/allow-bridge-nf-traffic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#sysctl net.bridge.bridge-nf-call-iptables=1
-cat >> /etc/sysctl.conf <<EOF
-  net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl -p /etc/sysctl.conf
